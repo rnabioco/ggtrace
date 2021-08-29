990f92a55a04d14dfe3c7948ae76fa174f833e45
Jay Hesselberth <jay.hesselberth@gmail.com>
2021-08-29 05:28:44 -0600

    Updates for CMD CHECK

---
+++ R/ggtrace-package.R
@@ -0,0 +1,28 @@
+#' ggtrace
+#'
+#' This package contains extra geoms for \pkg{ggplot2}.
+#'
+#' Please see the help pages listed below:
+#'
+#' \itemize{
+#'   \item \code{\link{geom_point_trace}}
+#' }
+#'
+#' Also see the vignette for more usage examples:
+#'
+#' \code{browseVignettes("ggtrace")}
+#'
+#' Please report issues and suggest improvements at Github:
+#'
+#' \url{https://github.com/rnabioco/ggtrace}
+#'
+#' @name ggtrace
+#' @docType package
+#' @import ggplot2
+#' @importFrom grid
+#'   gpar
+#'   pointsGrob
+#'   grobName
+#'   grobTree
+#' @keywords internal
+"_PACKAGE"
