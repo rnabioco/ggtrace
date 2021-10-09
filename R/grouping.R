58303a5232d3e823e3246d4d8c03437b19ae175a
rmsheridan <ryan.sheridan@ucdenver.edu>
2021-10-08 19:45:06 -0600

    potential group fix

--- R/grouping.R
+++ R/grouping.R
@@ -2,6 +2,80 @@
 # of plyr::id() used by add_group()
 NO_GROUP <- -1L
 
+id <- function(.variables, drop = FALSE) {
+  nrows <- NULL
+
+  if (is.data.frame(.variables)) {
+    nrows      <- nrow(.variables)
+    .variables <- unclass(.variables)
+  }
+
+  lengths    <- vapply(.variables, length, integer(1))
+  .variables <- .variables[lengths != 0]
+
+  if (length(.variables) == 0) {
+    n  <- nrows %||% 0L
+    id <- seq_len(n)
+
+    attr(id, "n") <- n
+
+    return(id)
+  }
+
+  if (length(.variables) == 1) {
+    return(id_var(.variables[[1]], drop = drop))
+  }
+
+  ids       <- rev(lapply(.variables, id_var, drop = drop))
+  p         <- length(ids)
+  ndistinct <- vapply(ids, attr, "n", FUN.VALUE = numeric(1), USE.NAMES = FALSE)
+  n         <- prod(ndistinct)
+
+  if (n > 2^31) {
+    char_id <- do.call("paste", c(ids, sep = "\r"))
+    res     <- match(char_id, unique(char_id))
+
+  } else {
+    combs <- c(1, cumprod(ndistinct[-p]))
+    mat   <- do.call("cbind", ids)
+    res   <- c((mat - 1L) %*% combs + 1L)
+  }
+
+  if (drop) {
+    id_var(res, drop = TRUE)
+
+  } else {
+    res <- as.integer(res)
+    attr(res, "n") <- n
+    res
+  }
+}
+
+id_var <- function(x, drop = FALSE) {
+
+  if (length(x) == 0) {
+    id <- integer()
+    n  <- 0L
+
+  } else if (!is.null(attr(x, "n")) && !drop) {
+    return(x)
+
+  } else if (is.factor(x) && !drop) {
+    x  <- addNA(x, ifany = TRUE)
+    id <- as.integer(x)
+    n  <- length(levels(x))
+
+  } else {
+    levels <- sort(unique(x), na.last = TRUE)
+    id     <- match(x, levels)
+    n      <- max(id)
+  }
+
+  attr(id, "n") <- n
+
+  id
+}
+
 # Ensure that the data frame contains a grouping variable.
 #
 # If the `group` variable is not present, then a new group
@@ -17,7 +91,7 @@ add_group <- function(data) {
     disc[names(disc) %in% c("label", "PANEL")] <- FALSE
 
     if (any(disc)) {
-      data$group <- vctrs::vec_group_id(data[disc])
+      data$group <- id(data[disc], drop = TRUE)
 
     } else {
       data$group <- NO_GROUP
@@ -25,7 +99,7 @@ add_group <- function(data) {
     }
 
   } else {
-    data$group <- vctrs::vec_group_id(data["group"])
+    data$group <- id(data["group"], drop = TRUE)
   }
 
   data
